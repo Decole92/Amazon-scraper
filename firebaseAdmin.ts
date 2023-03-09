74e181474e5bed088df24de872e127e76aae3b0d
Decole mills <okpoka92@gmail.com>
2023-03-09 11:10:22 +0100

    modifying

--- firebaseAdmin.ts
+++ firebaseAdmin.ts
@@ -1,8 +1,7 @@
 import * as admin from 'firebase-admin'
 import { getApps } from 'firebase-admin/app'
 
-const serviceAccount = require('./serviceAccountKey.json');
-
+const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY as string);
 if(!getApps().length){
     admin.initializeApp({
         credential: admin.credential.cert(serviceAccount),
